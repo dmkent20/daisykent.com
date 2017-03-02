6f9785c99d7e2483b1a28c842113c9d2e171dbaf
frunez <francesco.nunez@gmail.com>
2017-03-02 21:59:52 +0000

    set up posts controller and passed prompt test

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'posts' do
   context 'no posts have been added' do
     scenario 'should display a prompt to add a post' do
-      visit '/'
+      visit '/posts'
       expect(page).to have_content 'Time to write your first post :D'
     end
   end

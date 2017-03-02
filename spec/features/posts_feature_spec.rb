c46970c0a29b14f5f61744b93ef918d1dd99a87f
frunez <francesco.nunez@gmail.com>
2017-03-02 23:23:12 +0000

    fixing duplication error within database

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -13,4 +13,15 @@ feature 'posts' do
       expect(page).to have_link 'Create new post'
     end
   end
+  context 'posts have been created' do
+    before do
+      Post.create(title: 'Shoes')
+    end
+
+    scenario 'display posts' do
+      visit '/posts'
+      expect(page).to have_content('Shoes')
+      expect(page).not_to have_content('Time to write your first post :D')
+    end
+  end
 end

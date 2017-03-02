e2ac0b1384a63b4baa758815766749eca9eb2d42
frunez <francesco.nunez@gmail.com>
2017-03-02 22:14:12 +0000

    create dead link to create a new post

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -7,4 +7,10 @@ feature 'posts' do
       expect(page).to have_content 'Time to write your first post :D'
     end
   end
+  context 'creating a new post' do
+    scenario 'there should be a link to create a new post' do
+      visit '/posts'
+      expect(page).to have_link 'Create new post'
+    end
+  end
 end

0a81e27dc79135350e0e625efc49f5f5dad65d57
frunez <francesco.nunez@gmail.com>
2017-03-18 00:23:46 +0000

    added validation parameters to all models

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,5 @@
 class Admin < ApplicationRecord
+  validates :email, :password, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   has_and_belongs_to_many :categories
 
-  validates :title, length: {minimum: 1}
+  validates :title, presence: true, length: {minimum: 1}
 
   mount_uploader :blogheader, BlogHeaderUploader
 end

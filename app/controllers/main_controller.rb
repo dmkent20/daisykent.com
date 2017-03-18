5f26c200643fde3afe9ff65a54126aa3b6458aad
frunez <francesco.nunez@gmail.com>
2017-03-18 15:20:44 +0000

    I did stuff

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,7 @@ class MainController < ApplicationController
   layout "website"
 
   def home
-    @posts = Post.all
+    @posts = Post.all.reverse
     @post = Post.last
   end
 
@@ -13,11 +13,16 @@ class MainController < ApplicationController
   end
 
   def blog
+    @posts = Post.all.reverse
   end
 
   def portfolio
   end
 
   def videos
+  end
+
+  def blogpost
+    @post = Post.find(params[:id])
   end
 end
